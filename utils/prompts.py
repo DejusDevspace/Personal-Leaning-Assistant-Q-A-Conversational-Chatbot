c559a246bd34599fda02e85ca382b104ee2ef65d
Deju Adejo <ojomideju2003@gmail.com>
2024-08-07 01:39:22 +0100

    update prompts

--- utils/prompts.py
+++ utils/prompts.py
@@ -1,11 +1,11 @@
 # Contextualize question prompt
 CTQ_PROMPT = """
-Given a chat history and the latest user question which might make reference to the context in 
+Given the above conversation and the latest user question which might make reference to the context in 
 the chat history, generate a standalone question which can be understood without the chat history.
 
 Do NOT answer the question, just restructure it if needed and otherwise, return it as is.
 
-If there is no history, do not do anything, just say "no chat history".
+If there is no history, return the question as is.
 """
 
 # Question-answer system prompt
@@ -18,6 +18,9 @@ If you don't know the answer, just say that you don't know. Remember to ALWAYS p
 information and keep the answer concise.
 
 {context}
+
+Question: {input}
+Answer:
 """
 
 # Placeholder AI message
