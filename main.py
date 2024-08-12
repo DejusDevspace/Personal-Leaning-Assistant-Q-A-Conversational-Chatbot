9f5a1323a589f8eb4ab66f05ed97e483b60e53f7
Deju Adejo <ojomideju2003@gmail.com>
2024-08-12 01:51:05 +0100

    app: app

--- main.py
+++ main.py
@@ -3,62 +3,162 @@ from learning_assistant import PersonalLearningAssistant
 from utils import tools
 
 
-# assistant.route()
-st.title("Learning assistant")
-uploaded_file = st.file_uploader("upload file", type=["pdf", "docx"])
+def load_css(path: str) -> None:
+    """
+    Loads a custom css file at for styling
+    :param path: Path to the css file
+    :return: None
+    """
+    with open(path, "r") as file:
+        css = file.read()
 
-# -------------------- SESSION STATE VARIABLES -------------------- #
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = []
+    st.markdown(
+        f"<style>{css}</style>", unsafe_allow_html=True)
+    return None
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
 
-if "files" not in st.session_state:
-    st.session_state.files = []
+def main():
+    """Program entry function"""
+    # ------------------------------ PAGE CONFIGURATION ------------------------------ #
+    st.set_page_config(
+        page_title="Assistant",
+        page_icon="🤖",
+    )
 
-if "assistant" not in st.session_state:
-    st.session_state.assistant = PersonalLearningAssistant(model="llama3-70b-8192", temperature=0.4)
+    # Load styling from css file
+    load_css("assets/styles/main.css")
 
-# Display conversation history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    # ------------------------------ SIDEBAR ------------------------------ #
+    # st.title("Learning assistant 📝 ")
+    with st.sidebar:
+        st.title("Settings")
+        st.divider()
+        st.markdown("""
+                    This is the first version of this learning assistant. Current abilities include:
 
-if uploaded_file:
-    file_name = uploaded_file.name
-    file_type = uploaded_file.name.split(".")[-1]
-    # Load a new vectorstore for new document uploads
-    if file_name not in st.session_state.files:
-        file_path = tools.load_file_to_dir(uploaded_file, suffix=file_type)
-        documents = tools.process_file(file_path, file_type)
-        st.session_state.assistant.load_vectorstore_as_retriever(documents)
-        st.session_state.files.append(file_name)
+                    - Text Generation
+                    - Text Summarization
+                    - File Upload
+                    - Learning Assistance
 
-if prompt := st.chat_input("Enter a prompt..."):
-    # Display the user's message
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    # Display a loading sequence while generating response
-    with st.spinner("Loading response..."):
+                    More functionalities to be added...
+                    """)
+        st.divider()
+
+        # File upload
+        uploaded_file = st.file_uploader("Upload your slides in .pdf or .docx formats", type=["pdf", "docx"])
+        st.info(
+            "On first query about uploaded file, make reference to the file in your prompt",
+            icon=":material/info:")
+        st.divider()
+        st.markdown(
+            """
+            Visit my GitHub repository to view source code.
+
+            Click the button below 👇
+            """
+        )
+        st.link_button("DejusDevspace", "https://github.com/DejusDevspace")
+
+    # ----------------------------- PAGE CONTENT ------------------------------ #
+    st.markdown(
+        """
+        <div class="flex-container">
+        <div class="container">
+
+        </div>
+        <div class="container">
+
+        </div>
+        <div class="container">
+
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+    st.caption("Made with :heart: by Deju")
+
+    # SESSION STATE VARIABLES #
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    if "files" not in st.session_state:
+        st.session_state.files = []
+
+    if "assistant" not in st.session_state:
+        st.session_state.assistant = PersonalLearningAssistant(model="llama3-70b-8192", temperature=0.4)
+
+    # CONVERSATION CHAT HISTORY #
+    # Display conversation history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # FILE UPLOAD
+    if uploaded_file:
+        file_name = uploaded_file.name
+        file_type = uploaded_file.name.split(".")[-1]
+
+        # Load a new vectorstore for new document uploads
+        if file_name not in st.session_state.files:
+            file_path = tools.load_file_to_dir(uploaded_file, suffix=file_type)
+            documents = tools.process_file(file_path, file_type)
+            st.session_state.assistant.load_vectorstore_as_retriever(documents)
+            st.session_state.files.append(file_name)
+
+    if prompt := st.chat_input("Enter a prompt..."):
+        # Display the user's message
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        # Display a loading sequence while generating response
+        with st.spinner("Loading response..."):
+            try:
+                response = st.session_state.assistant.query(question=prompt, chat_history=st.session_state.chat_history)
+
+                # Display the AI's message
+                with st.chat_message("assistant"):
+                    stream = tools.stream_data(response)
+                    st.write_stream(stream)
+            except Exception as e:
+                print("Unable to load response:", e)
+                st.error(
+                    """
+                    Unable to provide response at this time.
+
+                    Possible fixes:
+
+                        • Check your internet connection.
+
+                        • Reload the page and try again.
+
+                        • If you uploaded a file, make sure it is in an accepted format.
+
+                    If the problem persists, please exit the page and try again later.
+                    """
+                )
+
         try:
-            response = st.session_state.assistant.query(question=prompt, chat_history=st.session_state.chat_history)
-            # Display the AI's message
-            with st.chat_message("assistant"):
-                stream = tools.stream_data(response)
-                st.write_stream(stream)
+            # Add new conversations to messages and chat history
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            st.session_state.messages.append({"role": "assistant", "content": response})
+
+            # Update the chat history
+            st.session_state.chat_history.extend([
+                {"type": "human", "content": prompt},
+                {"type": "assistant", "content": response}
+            ])
         except Exception as e:
-            print("Unable to load response:", e)
-            st.error("Unable to provide response at this time.")
+            print("Error:", e)
 
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.session_state.messages.append({"role": "assistant", "content": response})
-    st.session_state.chat_history.extend([
-        {"type": "human", "content": prompt},
-        {"type": "assistant", "content": response}
-    ])
+    # Removing some conversations to preserve context window
+    if len(st.session_state.chat_history) > 5:
+        # Get rid of the first message in the history when the messages are more than 5
+        st.session_state.chat_history.pop(0)
 
-# Removing some conversations to preserve context window size
-if len(st.session_state.chat_history) > 8:
-    # Get rid of the first message in the history when the messages get up to 8
-    st.session_state.chat_history.pop(0)
+
+if __name__ == "__main__":
+    main()
